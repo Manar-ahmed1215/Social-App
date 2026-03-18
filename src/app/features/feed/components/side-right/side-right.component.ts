6ed323a7af4a0936a5b4166d6d1fde4f04623c13
manarAhmed645 <mmony4544@gmail.com>
2026-03-18 05:13:18 +0200

    Fix dependencies for Vercel deployment

--- src/app/features/feed/components/side-right/side-right.component.ts
+++ src/app/features/feed/components/side-right/side-right.component.ts
@@ -1,96 +1,100 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { FollowersService } from '../../../../core/services/followers.service';
 import { Followers } from '../../../../core/models/followers.interface';
-import { debounceTime } from 'rxjs';
-import { FormControl, FormsModule } from '@angular/forms';
+import { Subject, debounceTime, distinctUntilChanged } from 'rxjs'; // أضف هذه
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
-
 
 @Component({
   selector: 'app-side-right',
+  standalone: true, // تأكد من وجودها إذا كنت تستخدم Angular 14+
   imports: [FormsModule, RouterLink],
   templateUrl: './side-right.component.html',
   styleUrl: './side-right.component.css',
 })
-export class SideRightComponent implements OnInit {
+export class SideRightComponent implements OnInit, OnDestroy {
+  private readonly followersService = inject(FollowersService);
+  private searchSubject = new Subject<string>();
 
-  private readonly followersService = inject(FollowersService)
-  searchControl = new FormControl('');
-  follower: Followers[] = []
-  page: number = 1
+  follower: Followers[] = [];
+  page: number = 1;
   loadingMore: boolean = false;
   loadingSuggestions: boolean = false;
   updatingFollow: string | null = null;
-  searchText: string = ""
+  searchText: string = "";
+
 
   ngOnInit(): void {
-    this.getFollowSuggestions()
-    this.inputChange()
-  }
-getFollowSuggestions(): void {
-  if (this.loadingSuggestions) return;
-  this.loadingSuggestions = true;
+   this.inputChange()
 
-  this.followersService.getFollowSuggestions(this.page, this.searchText).subscribe({
-    next: (res) => {
-      this.follower.push(...res.data.suggestions);
-      this.loadingSuggestions = false;
-    },
-    error: (err) => {
-      console.log(err);
-      this.loadingSuggestions = false;
-    }
-  });
-}
- loadMore(): void {
-  if (this.loadingMore || this.loadingSuggestions) return;
-
-  this.page++;
-  this.loadingMore = true;
-
-  this.followersService.getFollowSuggestions(this.page, this.searchText).subscribe({
-    next: (res) => {
-      this.follower.push(...res.data.suggestions);
-      this.loadingMore = false;
-    },
-    error: (err) => {
-      console.log(err);
-      this.loadingMore = false;
-    }
-  });
-}
-
-  followOrUnFollow(userId: string): void {
-    this.updatingFollow = userId
-    this.followersService.followOrUnfollow(userId).subscribe({
-      next: (res) => {
-        console.log(res)
-        this.follower = this.follower.filter(user => user._id !== userId);
-        this.updatingFollow = null
-
-      },
-      error: (err) => {
-        console.log(err)
-        this.updatingFollow = null
-      }
-    })
-  }
-
-  search() {
-    this.page = 1;
-    this.follower = [];
     this.getFollowSuggestions();
   }
-inputChange(): void {
-  this.searchControl.valueChanges
-    .pipe(debounceTime(700)) 
-    .subscribe(value => {
-      this.page = 1;
-      this.follower = [];
-      this.searchText = value ?? '';
-      if (this.loadingSuggestions) return;
 
-      this.getFollowSuggestions();
+
+  getFollowSuggestions(): void {
+    if (this.loadingSuggestions) return;
+    this.loadingSuggestions = true;
+
+    this.followersService.getFollowSuggestions(this.page, this.searchText).subscribe({
+      next: (res) => {
+        if (this.page === 1) {
+          this.follower = res.data.suggestions;
+        } else {
+          this.follower.push(...res.data.suggestions);
+        }
+        this.loadingSuggestions = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loadingSuggestions = false;
+      }
     });
-}
+  }
+  followOrUnFollow(userId: string): void {
+    this.updatingFollow = userId;
+    this.followersService.followOrUnfollow(userId).subscribe({
+      next: () => {
+        this.follower = this.follower.filter(user => user._id !== userId);
+        this.updatingFollow = null;
+      },
+      error: () => {
+        this.updatingFollow = null
+      }
+    });
+  }
+
+  loadMore(): void {
+    if (this.loadingMore || this.loadingSuggestions) return;
+    this.page++;
+    this.loadingMore = true;
+    this.followersService.getFollowSuggestions(this.page, this.searchText).subscribe({
+      next: (res) => {
+        this.follower.push(...res.data.suggestions);
+        this.loadingMore = false;
+      },
+      error: (err) => {
+        this.loadingMore = false;
+      }
+    });
+  }
+
+  search(): void {
+    this.searchSubject.next(this.searchText);
+  }
+  ngOnDestroy(): void {
+    this.searchSubject.complete();
+  }
+ inputChange(): void{
+   this.searchSubject.pipe(
+      debounceTime(500), 
+      distinctUntilChanged() 
+    ).subscribe(value => {
+      this.searchText = value; 
+      this.page = 1;      
+      this.follower = [];      
+      this.getFollowSuggestions(); 
+    });
+ }
+
+  
 }
